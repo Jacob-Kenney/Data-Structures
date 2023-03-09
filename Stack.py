9cda099984fd136073c6c960d1f74c93f8240c99
Jacob Kenney <69850434+Jacob-Kenney@users.noreply.github.com>
2023-03-09 11:23:28 +0000

    Completed code for stack

--- Stack.py
+++ Stack.py
@@ -1,5 +1,5 @@
 #Class for Node
-class Node
+class Node:
 
   #function to initialise an instance of node, taking data as input
   def __init__(self,data):
@@ -14,24 +14,40 @@ class Node
     self.next_node = next_node
     
 #Class for Stack
+class Stack:
 
   #function to initialise an instance of stack, taking no input
+  def __init__(self):
     #set stack instance variable, head, to None
+    self.head_node = None
     
   #function to push (add a new node to stack head), taking value as input
+  def push(self, value):
     #assign a temporary variable (new head) to a Node instance with data set to value input
+    new_head = Node(value)
     #if stack head instance variable is None
+    if self.head_node == None:
       #set stack head instance variable to new head
+      self.head_node = new_head
     #else
+    else:
       #set the next node instance variable of new head to the head node of this stack
+      new_head.set_next_node(self.head_node)
       #set stack head instance variable to new head
+      self.head_node = new_head
   
   #function to pop (take a node from the stack head), taking no input
+  def pop(self):
     #if head of this instance of stack is not None
+    if self.head:
       #assign a temporary variable (current head) to the head instance variable of this stack
+      current_head = self.head_node
       #set stack head instance variable to the next node instance variable of current head
+      self.head_node = current_head.next_node
       #return current head
+      return current_head
     #return None
+    return None
   
   #function to peek (view head without modifying the state of the queue), taking no input
   def peek(self):
